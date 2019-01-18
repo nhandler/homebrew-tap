8c78244db77908e723556b6b87ce128a08681168
Nathan Handler <nhandler@orchid.com>
2019-01-18 11:12:37 -0800

    Add pdf-to-keynote Cask

---
+++ Casks/pdf-to-keynote.rb
@@ -0,0 +1,11 @@
+cask 'pdf-to-keynote' do
+  version '1.04a'
+  sha256 '3703dae673e8e3cea86b9eed36e8a9d7dc679c16f06ef3616bf1adb86c0b09a9'
+
+  url 'https://www.cs.hmc.edu/~oneill/freesoftware/PDFtoKeynote-v1.04a.dmg'
+  appcast 'https://www.cs.hmc.edu/~oneill/freesoftware/pdftokeynote.html#release-notes'
+  name 'PDF to Keynote'
+  homepage 'https://www.cs.hmc.edu/~oneill/freesoftware/pdftokeynote.html'
+
+  app 'PDF to Keynote.app'
+end
